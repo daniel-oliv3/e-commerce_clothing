3f1f2043970db5909488ae986fdb0de83643dd45
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-24 19:46:46 -0300

    Slider product-card

---
+++ js/home.js
@@ -0,0 +1,16 @@
+const productContaineres = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContaineres.forEach((item, i) => {
+    let containerDimenstions = item.getBoundingClientRect();
+    let containerWidth = containerDimenstions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
