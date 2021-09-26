494ee70b47212ecf6a6a5cc7f34f37cee487a2b7
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-26 00:16:20 -0300

    Toggle-Size-Buttons

--- js/product.js
+++ js/product.js
@@ -11,3 +11,16 @@ productImages.forEach((item, i) => {
         activeImageSlide = i;
     })
 })
+
+// botões de alternância de tamanho
+const sizeBtn = document.querySelectorAll(".size-radio-btn");
+
+let checkedBtn = 0;
+
+sizeBtn.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        sizeBtn[checkedBtn].classList.remove('check');
+        item.classList.add('check');
+        checkedBtn = i;
+    })
+})
