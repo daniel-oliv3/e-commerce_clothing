0420b0b17d8d10f32b5313119217e0dd8a3e5c62
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-26 00:10:18 -0300

    Product Images Slider

---
+++ js/product.js
@@ -0,0 +1,13 @@
+const productImages = document.querySelectorAll(".product-images img");
+const productImageSlide = document.querySelector(".image-slider");
+
+let activeImageSlide = 0;
+
+productImages.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        productImages[activeImageSlide].classList.remove('active');
+        item.classList.add('active');
+        productImageSlide.style.backgroundImage = `url('${item.src}')`;
+        activeImageSlide = i;
+    })
+})
